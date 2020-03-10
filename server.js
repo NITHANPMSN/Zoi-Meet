955dd7137df0958b97121f657a5b593c930f2a3c
ptsathish <sathishyuvaraj007@gmail.com>
2020-03-10 21:35:48 +0530

    Initial commit.

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const accountSid = 'AC6690f02796407c0346bd5863bcc26519';
+const authToken = '5e74bfe626f29fee8782dea660e13c65';
+const client = require('twilio')(accountSid, authToken);
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/post', (req, res) => {
+  console.log(req, 'req');
+  res.send({ message: 'received' });
+})
+
+app.get('/call', (req, res) => {
+  client.calls
+    .create({
+      record: true,
+      url: 'http://demo.twilio.com/docs/voice.xml',
+      to: '+918489517576',
+      from: '+441233801192'
+    })
+    .then(call => {
+      console.log(call)
+      client.calls(call.sid).recordings.create().then(rec =>
+        console.log(rec, 'rec')
+        //   client.recordings(`${rec.sid}`)
+        //     .fetch()
+        //     .then(msg => console.log(msg, 'msg'))
+        //     .catch(err => console.log('err', err))
+      )
+    })
+    .catch(err => console.log(err));
+  res.send({ message: 'Call connected...' });
+})
+
+app.listen(5000, () => {
+  console.log('server running')
+})
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,134 @@
-import React from "react"
-import { Link } from "gatsby"
-
+import React, { useState } from "react"
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import { ReactMic } from 'react-mic';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+let audio;
+let interval;
+const audioChunks = [];
+let audioBlob;
 
+
+const styles = {
+  button: {
+    padding: '20px',
+    borderRadius: '20px'
+  },
+  buttonBox: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  audio: {
+    backgroundColor: 'gray',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  twilioBox: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    padding: '20px'
+  },
+}
+
+const IndexPage = () => {
+
+  const [isRecording, setIsRecording] = useState(false);
+  const [size, setSize] = useState(0);
+
+
+  const onData = async (e) => {
+    await audioChunks.push(e);
+  }
+
+  const onStop = async () => {
+    const audioUrl = URL.createObjectURL(audioBlob);
+    audio = new Audio(audioUrl);
+  }
+
+
+  const play = () => {
+    if (audio) audio.play();
+  }
+
+
+
+  function bytesToSize(bytes) {
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes == 0) return '0 Byte';
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+  }
+
+  const record = () => {
+    interval = setInterval(() => {
+      send()
+    }, 5000);
+    setIsRecording(true);
+  }
+
+  const pause = () => {
+    clearInterval(interval);
+    setIsRecording(false);
+  }
+
+  const twilio = () => {
+    fetch('http://localhost:5000/call', {
+      method: 'GET',
+    }).then(data => console.log(data)).catch(err => console.log(err))
+  }
+
+  const send = async () => {
+    audioBlob = new Blob(audioChunks);
+    let temp = audioBlob.size;
+    setSize(bytesToSize(temp));
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(audioBlob),
+      mode: 'no-cors',
+      headers: { 'content-type': '*' }
+    }
+    const response = await fetch('http://localhost:5000/post', options);
+  }
+
+
+  return (
+    <Layout>
+      <div style={styles.audio}>
+        <h4>Browser-audio</h4>
+        <ReactMic
+          record={isRecording}
+          className="sound-wave"
+          onStop={onStop}
+          onData={onData}
+          strokeColor="#000000"
+          backgroundColor="#FF4081"
+        />
+        <br />
+
+        <div style={styles.buttonBox}>
+
+          <button style={styles.button} onClick={record} >Rec</button>
+
+          <button style={styles.button} onClick={pause}>Stop</button>
+
+          <button style={styles.button} onClick={play}>play</button>
+
+          <button style={styles.button} disabled>{size}</button>
+
+        </div>
+      </div>
+      <br />
+
+      <div style={styles.audio}>
+        <h4>Twilio-voice</h4>
+        <div style={styles.twilioBox}>
+          <input placeholder='Enter number...'></input>
+          <button onClick={twilio}>call</button>
+        </div>
+      </div>
+
+    </Layout>
+  )
+}
 export default IndexPage
